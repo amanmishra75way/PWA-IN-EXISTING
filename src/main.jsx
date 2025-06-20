5cea9d88db658f2cf3cf4d0bdd463fb9c3ec4f86
amanmishra75way <amishra.75way@gmail.com>
2025-06-20 13:13:04 +0530

    Refactor service worker registration and update manifest icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+// Register the service worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("Service Worker registration failed: ", registrationError);
+      });
+  });
+}
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
